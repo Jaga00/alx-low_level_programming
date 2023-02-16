5ab926378e07bcaf752769573de1a27e6535ccb5
Jaga00 <jamessimba07@gmail.com>
2023-02-16 15:19:46 -0800

    size matter

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,9 @@ int main(void)
 {
 	printf("Size of a char: %u byte(s)\n", sizeof(char));
 	printf("Size of an int: %u byte(s)\n", sizeof(int));
-	printf("Size of a long: %u byte(s)\n", sizeof(long));
-	printf("Size of a long long: %u byte(s)\n", sizeof(long long));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long));
 	printf("Size of a float: %u byte(s)\n", sizeof(float));
-	printf("Size of a double: %u byte(s)\n", sizeof(double));
-	printf("Size of a long double: %u byte(s)\n", sizeof(long double));
 	return (0);
 }
 
