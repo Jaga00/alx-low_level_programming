d94e16b3bb5ebefd8ce25b604740929a3d6d74eb
Jaga00 <jamessimba07@gmail.com>
2023-02-17 05:16:47 -0800

    pone

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: 'pos or neg'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand()  - RAND_MAX / 2;
+
+	printf("%d \n", n);
+
+	return (0);
+}
