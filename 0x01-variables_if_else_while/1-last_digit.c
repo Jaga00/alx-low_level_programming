001a4124a74d3a6bd211e27c1f5a4b19a22a2482
Jaga00 <jamessimba07@gmail.com>
2023-02-20 01:08:36 -0800

    Pone

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,19 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	y = n % 10;
-	printf("The last digit of the number %d is %d \n", n, y);
+	if (y > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, y);
+	}
+	else if (y < 6 && y != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
+	}
+	else
+	{
+	printf("Last digit of %d is %d and is 0\n", n, y);
+	}
+
 
 	return (0);
 }
