58f5c84bafbccb896427e8628eada4f7235497d7
Jaga00 <jamessimba07@gmail.com>
2023-02-20 05:10:57 -0800

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'the combs'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i; j < 10; j++)
+		putchar(i + '0');
+		if (i < 9)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 9 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
