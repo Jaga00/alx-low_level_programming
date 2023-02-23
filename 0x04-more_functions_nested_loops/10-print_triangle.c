96e1b31e08736ff759335e7fa92ec7291bc7912f
Jaga00 <jamessimba07@gmail.com>
2023-02-23 11:42:17 -0800

    on screen triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - check for a digit
+ * @size : integer
+ * Return:void
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
