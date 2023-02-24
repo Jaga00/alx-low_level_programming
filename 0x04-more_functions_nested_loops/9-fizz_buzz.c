6793e0565ef14f83245aa5bfab73e38a6861eb1a
Jaga00 <jamessimba07@gmail.com>
2023-02-24 14:04:44 -0800

    fizzbuzzing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,26 +3,26 @@
 /**
  * main - Fizz Buzz code
  *
- * Return: Always 0 (Success)
+ * Return: void
  */
 
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	for (i = 1; i <= 100; i++)
+	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
